b7acbb2e7fde47c559bd06d01ebcaf7764db8c8f
Aleyna Sueda Guzel <aleynasuedaguzel@gmail.com>
2024-09-28 17:42:05 +0300

    kullanıcı bilgileri ve gönderiler oluşturuldu

---
+++ JavaScript_recap1/index.js
@@ -0,0 +1,22 @@
+var kullanicilar = [
+    {email : "aleyna@gmail.com", sifre : "12345"},
+    {email : "sueda@gmail.com", sifre : "12345"}
+]
+
+var gonderiler = [
+    {email: "aleyna@gamil.com", gonderi: "hava cok guzel"},
+    {email: "aleyna@gamil.com", gonderi: "hava cok guzel 2"},
+    {email: "sueda@gamil.com", gonderi: "hava cok guzel"}
+]
+
+function giris(){
+    if((email == kullanicilar[0].email && sifre==kullanicilar[0].sifre )||
+    (email == kullanicilar[1].email && sifre==kullanicilar[1].sifre )){
+        console.log(gonderiler)
+    }
+    else{
+    console.log("Giriş Hatalı!")
+    }
+}
+
+giris(email,sifre)
