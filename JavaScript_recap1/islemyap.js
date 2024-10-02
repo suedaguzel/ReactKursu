6c8ec84380f9c2a2b775dcf4e23d57701a7aae43
Aleyna Sueda Guzel <aleynasuedaguzel@gmail.com>
2024-10-02 16:29:52 +0300

    işlemlerde yapılan hatalar düzeltildi

--- JavaScript_recap1/islemyap.js
+++ JavaScript_recap1/islemyap.js
@@ -1,22 +1,22 @@
-var sayi1 = prompt("Birinci Sayı: ")
-var sayi2 = prompt("İkinci Sayı: ")
+var sayi1 = parseFloat(prompt("Birinci Sayı: "))
+var sayi2 = parseFloat(prompt("İkinci Sayı: "))
 var islem = prompt("Yapmak istediğiniz işlemi yazınız (bol, carp, topla, cikar): ")
 
 function hesapla(sayi1,sayi2,islem){
-    if(islem===bol){
+    if(islem==='bol'){
         return sayi1/sayi2
     }
-    else if(islem===carp){
+    else if(islem==='carp'){
         return sayi1*sayi2
     }
-    else if(islem===topla){
+    else if(islem==='topla'){
         return sayi1+sayi2
     }
-    else if(islem === cikar){
+    else if(islem === 'cikar'){
         return sayi1-sayi2
     }else
         return "Geçersiz İşlem"
 }
 
 var sonuc = hesapla(sayi1, sayi2, islem)
-console.log(sonuc)
+console.log("Sonuc: ",sonuc)
