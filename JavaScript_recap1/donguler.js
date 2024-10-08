c0c1fb3276df8c9b62a361fa3766cecb4ee02152
Aleyna Sueda Guzel <aleynasuedaguzel@gmail.com>
2024-10-08 13:14:28 +0300

    döngüler denendi

---
+++ JavaScript_recap1/donguler.js
@@ -0,0 +1,18 @@
+// for (i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+var sehirler = ["Ankara", "İstanbul", "Bursa", "Adana"];
+
+// for (i = 0; i < sehirler.length; i++) {
+//   console.log(sehirler[i]);
+// }
+
+// var i = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+sehirler.forEach(function(sehir){
+    console.log(sehir)
+})
